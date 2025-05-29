e4fa49c25d22e6b6e6926e68364b3094c1fa40a7
tsomchan <marvin@42.fr>
2025-05-29 15:57:21 +0700

    free images and textures

--- src/free.c
+++ src/free.c
@@ -45,12 +45,20 @@ void	free_node(t_node **node)
 	}
 }
 
-void	free_tx(t_tx *tx)
+void	free_tx(t_tx *tx, mlx_t *mlx)
 {
 	free_if_alloc(tx->no);
 	free_if_alloc(tx->so);
 	free_if_alloc(tx->ea);
 	free_if_alloc(tx->we);
+	mlx_delete_image(mlx, tx->no_img);
+	mlx_delete_image(mlx, tx->so_img);
+	mlx_delete_image(mlx, tx->we_img);
+	mlx_delete_image(mlx, tx->ea_img);
+	mlx_delete_texture(tx->no_tx);
+	mlx_delete_texture(tx->so_tx);
+	mlx_delete_texture(tx->we_tx);
+	mlx_delete_texture(tx->ea_tx);
 	free(tx);
 }
 
@@ -58,19 +66,17 @@ void	free_stuff(t_data *data)
 {
 	if (data)
 	{
-		free_if_alloc(data->mlx);
 		free_if_alloc(data->player);
 		if (data->map)
 			free_map(data->map);
 		if (data->node != NULL)
 			free_node(&data->node);
 		if (data->tx)
-			free_tx(data->tx);
-		// if (data->ray)
-		// 	free_ray(data->ray);
+			free_tx(data->tx, data->mlx);
 		free_if_alloc(data->line);
 		free_if_alloc(data->f);
 		free_if_alloc(data->c);
+		mlx_terminate(data->mlx);
 		free(data);
 	}
 }

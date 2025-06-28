bbf55c4626fb20a66f3dae9aa962f8fc2948a35f
tsomchan <marvin@42.fr>
2025-06-28 17:46:46 +0700

    fix horizontal invert texture render

--- src/raycast/ray_utils.c
+++ src/raycast/ray_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ray_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paradari <bellixz610@gmail.com>            +#+  +:+       +#+        */
+/*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 00:38:03 by paradari          #+#    #+#             */
-/*   Updated: 2025/06/28 09:42:19 by paradari         ###   ########.fr       */
+/*   Updated: 2025/06/28 17:46:40 by tsomchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,9 +63,8 @@ void	ft_cal_value_wallx(t_ray *ray, t_player *player)
 		ray->wallx = player->x + ray->prep_wall_dist * ray->dir_x;
 	ray->wallx -= floor(ray->wallx);
 	ray->tx_x = (int)(ray->wallx * (double)SIZE);
-	if (ray->side == 0 && ray->dir_x > 0)
-		ray->tx_x = SIZE - ray->tx_x - 1;
-	if (ray->side == 1 && ray->dir_y < 0)
+	if ((ray->side == 0 && ray->dir_x < 0)
+		|| (ray->side == 1 && ray->dir_y > 0))
 		ray->tx_x = SIZE - ray->tx_x - 1;
 }
 
